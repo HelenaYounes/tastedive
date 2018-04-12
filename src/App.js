1fc0d5e5b92b71b7810c218135754bb4a4588c69
Héléna Younes <younesq.helena@gmail.com>
2018-04-12 09:58:26 -0700

    fetch data for similar tastes request

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import api from './api.json';
+import { Card } from 'antd';
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        similar: {},
+        results: [{
+          Name: "efwefw",
+          Type: "rijer"
+        }],
+      };
+  }
+
+
+
+  componentWillMount() {
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url = `https://tastedive.com/api/similar?q=red+hot+chili+peppers%2C+pulp+fiction&k=${api.key}`;
+    fetch(proxyurl + url)
+      .then(response => response.json())
+      .then(res => this.setState({results: res.Similar.Results}))
+
+
+  }
   render() {
+    const similars = this.state.results;
+    debugger
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="body_app">
+          {
+            similars.map((result, i) => {
+              return <Card title={result.Name} key={i}>
+                <p>{result.Type}</p>
+              </Card>
+            })
+          }
+        </div>
       </div>
     );
   }
 }
-
-export default App;
